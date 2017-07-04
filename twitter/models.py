eb4597cd220e3323a605ca2a881b92e31f0392bb
SadeghHayeri <hayerisadegh@gmail.com>
2017-07-04 21:44:55 +0430

    main models added!

--- twitter/models.py
+++ twitter/models.py
@@ -1,4 +1,14 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tweet(models.Model):
+    replyTo = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date = models.DateTimeField('date published')
+    body = models.CharField(max_length=140)
+
+class Like(models.Model):
+    post = models.OneToOneField(Tweet, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    likedTime = models.DateTimeField('date liked')
