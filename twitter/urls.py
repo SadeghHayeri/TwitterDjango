c27767a9d5a68ef44c3d73dbe7c1fc92c2cf7e20
SadeghHayeri <hayerisadegh@gmail.com>
2017-07-05 01:59:24 +0430

    REST api!

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'tweets', views.TweetViewSet)
+
 urlpatterns = [
-    url(r'', ),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tweet, Like
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, TweetSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class TweetViewSet(viewsets.ModelViewSet):
+    queryset = Tweet.objects.all()
+    serializer_class = TweetSerializer
+
+def index(request):
+    # lastTweets = Tweet.
+    return HttpResponse("Hi man!")
