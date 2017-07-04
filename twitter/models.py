85772c92f293d4ae401aa4123c72785433d89a3f
SadeghHayeri <hayerisadegh@gmail.com>
2017-07-05 00:48:05 +0430

    fix bad relation!

--- twitter/models.py
+++ twitter/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Tweet(models.Model):
-    replyTo = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    inherit = models.ForeignKey('self', related_name='children', null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField('date published')
     body = models.CharField(max_length=140)
 
 class Like(models.Model):
-    post = models.OneToOneField(Tweet, on_delete=models.CASCADE)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     likedTime = models.DateTimeField('date liked')
